d13f2583d05bd5c54e74ee45d7d36555b2efc2f0
Maria Radmer Almind <raal@itu.dk>
2022-09-26 09:17:50 +0200

    working on monte carlo...

--- labs/Work copies on labs/lab1 - monteCarlo.py
+++ labs/Work copies on labs/lab1 - monteCarlo.py
@@ -6,6 +6,7 @@ from typing import List, Tuple
 import time
 from copy import deepcopy  # world -> thought
 import time
+from unicodedata import name
 # world and world model
 
 
@@ -104,11 +105,12 @@ class Node:
         self.action = None
         self.tried = False
         self.untried = state.get_avail_actions()
+        self.player = True  # if X/max True else O/min False
 
         self.q: float = 0
         self.wins: int = 0
         self.visits: int = 0
-
+        self.ucb = 0
         self.c = sqrt(2)
 
     def nonTerminal(self):
@@ -119,20 +121,22 @@ class Node:
         return ((len(self.children)) < (actions))
 
     def qValue(self):
-        p: Node = deepcopy(self.parent)
+        p = self.parent
         if p != None:
-
-            tmp = sqrt((2*math.log(p.visits + 1)) / self.visits)
-            self.q = (self.wins/self.visits) + self.c * tmp
-            return self.q
+            tmp = sqrt((2*math.log(p.visits+1)) / self.visits)
+            self.q = self.wins/self.visits
+            self.ucb = (self.q/self.visits) + self.c * \
+                tmp  # gange med - eller +
+            return self.ucb
         return 0
 
     def bestChild(self):
-        self.children.sort(key=lambda x: x.q, reverse=True)
+        self.children.sort(key=lambda x: x.q, reverse=self.player)
+
         return self.children[0]
 
     def __repr__(self):
-        return f"Wins {self.wins} visits {self.visits} q-value {self.q} action {self.action}\n"
+        return f"Wins {self.wins} visits {self.visits} q-value {round(self.q,3)} ucb-value {round(self.ucb,3)} action {self.action}\n"
 
 
 class MCTS(Agent):
@@ -157,20 +161,20 @@ class MCTS(Agent):
     def search(self):
 
         state_init = deepcopy(self.state)
-        self.v_0 = Node(state_init, None)
+        root = Node(state_init, None)
 
         start = time.time()
         decision_time = 2
 
         while (utility(self.state) == 0 and not state_init.is_over() and (time.time() < start + decision_time)):
-            v_1 = self.treePolicy(self.v_0)
-            stateThink = deepcopy(self.state)
+            v_1 = self.treePolicy(root)
+            stateThink = deepcopy(state_init)
 
             delta = self.defaultpolicy(stateThink)
 
             self.backup(v_1, delta)
 
-        bestChild: Node = self.v_0.bestChild()
+        bestChild: Node = root.bestChild()
         return bestChild.action
 
     def treePolicy(self, node: Node):
@@ -180,17 +184,18 @@ class MCTS(Agent):
             if node.notFullyExpanded():
                 self.expand(node, self.state)
             else:
-                v = node.bestChild()
+                v = node.bestChild()  # all q-values are 0???
                 break
         return v
 
     def expand(self, node: Node, state: State):
         action = node.untried.pop(0)
 
-        stateThink = deepcopy(self.state)
-        stateThink.put_action(action, Agent('X'))
+        new_state = deepcopy(state)
+        new_state.put_action(action, Agent(self.getPlayerName()))
 
-        v_ = Node(stateThink, node)
+        v_ = Node(new_state, node)
+        v_.player = not node.player
         v_.action = action
         node.children.append(v_)
 
@@ -198,16 +203,13 @@ class MCTS(Agent):
 
     def defaultpolicy(self, stateThink: State):
 
-        #stateThink = deepcopy(self.state)
-
         while (not stateThink.is_over()):
-            self.player *= -1
+
             actions = stateThink.get_avail_actions()
             action = actions.pop(random.randrange(0, len(actions)))
-
             stateThink.put_action(action, Agent(self.getPlayerName()))
-
-        return (utility(stateThink)*self.player)  # is this correct ?
+            self.player *= -1
+        return (utility(stateThink))  # is this correct ?
 
     def backup(self, node: Node, delta):
         v = node
@@ -218,8 +220,9 @@ class MCTS(Agent):
             if delta > 0:
                 v.wins += 1
 
-            v.q = v.qValue() + delta
+            v.q = (v.qValue() + delta)
             v = v.parent
+            self.player *= -1
 
 
 agents = (Agent('O'), MCTS('X'))
